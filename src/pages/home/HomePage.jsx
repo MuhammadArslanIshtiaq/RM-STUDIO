eebcec4b42e34d90916110e8722b643f132244e3
Muhammad Arslan <muhammadarslanishtiaq@gmail.com>
2022-03-12 22:06:21 +0500

    responsiveness issues

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -6,20 +6,23 @@ const HomePage = () => {
     <main className="home-page">
       <ReactFullpage
         //fullpage options
+
         navigation
         licenseKey={"YOUR_KEY_HERE"}
-        scrollingSpeed={1000} /* Options here */
+        scrollingSpeed={1000}
+        responsiveWidth={1000}
+        /* Options here */
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
-              <div className="section bg-sec1 vh-100 fp-tableCell"></div>
-              <div className="section container-fluid">
+              <div className="section bg-sec1 vh-100 fp-auto-height-responsive"></div>
+              <div className="section container-fluid fp-auto-height-responsive">
                 <div class="row">
                   <div className="col-md-12 col-lg-6 bg-sec2a vh-100 br-sec2"></div>
                   <div className="col-md-12 col-lg-6 bg-sec2b vh-100 bl-sec2"></div>
                 </div>
               </div>
-              <div className="section bg-light vw-100 fp-tableCell">
+              <div className="section bg-light vw-100 fp-auto-height-responsive">
                 <div className="d-flex justify-content-center align-items-center">
                   <video
                     playsinline="playsinline"
@@ -36,13 +39,13 @@ const HomePage = () => {
                   </video>
                 </div>
               </div>
-              <div className="section container-fluid">
+              <div className="section container-fluid fp-auto-height-responsive">
                 <div class="row">
                   <div className="col-md-12 col-lg-6 bg-sec4a vh-100"></div>
                   <div className="col-md-12 col-lg-6 bg-sec4b vh-100"></div>
                 </div>
               </div>
-              <div className="section container-fluid">
+              <div className="section container-fluid fp-auto-height-responsive">
                 <div class="row">
                   <div className="col-md-12 col-lg-6 bg-sec5a vh-100"></div>
                   <div className="col-md-12 col-lg-6 bg-sec5b vh-100"></div>
