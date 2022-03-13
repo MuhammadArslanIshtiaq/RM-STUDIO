e59d94a8f0302bda06bd1850a73b22ceb87a4125
Muhammad Arslan <muhammadarslanishtiaq@gmail.com>
2022-03-13 13:25:40 +0500

    Video fixed

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Player, Ui, Video } from "@vime/react";
-import "video-player-for-react/dist/index.css";
 import ReactFullpage from "@fullpage/react-fullpage";
 import FooterPrimary from "../../components/footers/FooterPrimary";
 const HomePage = () => {
@@ -8,46 +6,39 @@ const HomePage = () => {
     <main className="home-page">
       <ReactFullpage
         //fullpage options
-
         navigation
         licenseKey={"YOUR_KEY_HERE"}
-        scrollingSpeed={1000}
-        responsiveWidth={1000}
-        /* Options here */
+        scrollingSpeed={1000} /* Options here */
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
               <div className="section bg-sec1 vh-100 fp-tableCell"></div>
-              <div className="section container-fluid fp-auto-height-responsive">
+              <div className="section container-fluid">
                 <div class="row">
                   <div className="col-md-12 col-lg-6 bg-sec2a vh-100 br-sec2"></div>
                   <div className="col-md-12 col-lg-6 bg-sec2b vh-100 bl-sec2"></div>
                 </div>
               </div>
-              <div className="section bg-light vw-100 fp-auto-height-responsive">
-                <div className="container d-flex justify-content-center align-items-center py-5 ">
-                  <Player controls autoplay loop muted>
-                    <Video
-                      crossOrigin=""
-                      poster="../../assets/backgrounds/video-bg.png"
-                    >
-                      <source
-                        data-src="../../assets/backgrounds/bg-hero.mp4"
-                        type="video/mp4"
-                      />
-                    </Video>
-
-                    {/* ... */}
-                  </Player>
+              <div className="section bg-light vw-100 fp-tableCell">
+                <div className="d-flex justify-content-center align-items-center">
+                  <video
+                    poster="../../assets/backgrounds/video-bg.png"
+                    src="../../assets/backgrounds/bg-hero.mp4"
+                    muted="muted"
+                    playsinline=""
+                    loop="loop"
+                    data-autoplay=""
+                    className="myvideo"
+                  ></video>
                 </div>
               </div>
-              <div className="section container-fluid fp-auto-height-responsive">
+              <div className="section container-fluid">
                 <div class="row">
                   <div className="col-md-12 col-lg-6 bg-sec4a vh-100"></div>
                   <div className="col-md-12 col-lg-6 bg-sec4b vh-100"></div>
                 </div>
               </div>
-              <div className="section container-fluid fp-auto-height-responsive">
+              <div className="section container-fluid">
                 <div class="row">
                   <div className="col-md-12 col-lg-6 bg-sec5a vh-100"></div>
                   <div className="col-md-12 col-lg-6 bg-sec5b vh-100"></div>
